9b7205d6b866a403a65f6337c035d0378c30a26a
Inkvitop <plinkvitop@gmail.com>
2021-10-29 23:36:52 +0200

    refactor: refactoring code MainFirstTask

--- src/MainTasks/MainFirstTask.java
+++ src/MainTasks/MainFirstTask.java
@@ -1,5 +1,8 @@
 package MainTasks;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.Scanner;
@@ -9,31 +12,40 @@ import java.util.Scanner;
 public class MainFirstTask {
     public static void main(String[] args) {
         System.out.println("Please, enter your name:");
-        newNameInput();
+        printResult(newNameInput());
     }
+
     public static boolean regexCheck(String name) {
         String regex = "\\W+|\\d";
         boolean flag;
 
-        //  Only text check
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(name);
         flag = matcher.find();
         return flag;
     }
-    public static void newNameInput() {
-        boolean regexCheckFlag;
+
+    public static ArrayList<String> newNameInput() {
         String name;
 
         Scanner in = new Scanner(System.in);
         name = in.nextLine();
-        regexCheckFlag = regexCheck(name);
 
-        if (!regexCheckFlag) {
-            System.out.println("Greetings " + name + "!");
+        ArrayList<String> result = new ArrayList<String>();
+        result.add(name);
+        result.add(String.valueOf(regexCheck(name)));
+        return result;
+    }
+
+    public static void printResult(ArrayList<String> result) {
+        System.out.println("!!!!!!!!!" + result);
+        if (Objects.equals(result.get(1), "false")) {
+            System.out.println("Greetings " + result.get(0) + "!");
         } else  {
             System.out.println("The name must consist of letters. Please try again!");
-            newNameInput();
+            System.out.println("----------------------------");
+            System.out.println("Please, enter your name:");
+            printResult(newNameInput());
         }
     }
 }
